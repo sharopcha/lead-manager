c94dc54a1be378abc08af6e5ffe9dc5d81209bd9
sharopcha <napster1202@gmail.com>
2020-09-09 19:42:30 +0300

    setting up a database and models

---
+++ leadmanager/leads/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
