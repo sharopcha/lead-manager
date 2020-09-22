b305bc8b3e0baf003fafd4bd26b35258bfe18b91
sharopcha <napster1202@gmail.com>
2020-09-22 07:41:09 +0300

    redirect user after register

--- leadmanager/frontend/src/components/accounts/Register.js
+++ leadmanager/frontend/src/components/accounts/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { register } from '../../actions/auth';
@@ -41,6 +41,10 @@ export class Register extends Component {
   };
 
   render() {
+    if (this.props.isAuthenticated) {
+      return <Redirect to='/' />;
+    }
+
     const { username, email, password, password2 } = this.state;
     return (
       <div className='col-md-6 m-auto'>
